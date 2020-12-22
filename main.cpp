37976fbb33d4553c991d13ebbaebe3d1d831aa44
zenehu <70635027+zeronethunter@users.noreply.github.com>
2020-12-22 21:52:00 +0300

    Update LR2

--- main.cpp
+++ main.cpp
@@ -1,25 +1,21 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
-double factorial(long int n) {
-    if (n == 0){
-        return 1;
-    }
-    return n * factorial(n-1);
-}
 int main() {
-
-    for(unsigned short int i = 0; i<=127; i++){
+    long long fast = 0;
+    for (unsigned short int i = 0; i <= 127; i++) {
         std::cout << static_cast<char>(i) << std::endl;
     }
     std::cout << "Input symbol: ";
     char sym;
     std::cin >> sym;
-    if ((sym >= 48) && (sym<=57)) {
+    if ((sym >= 48) && (sym <= 57)) {
         std::cout << " It's Digit" << std::endl;
-    } else if (((sym>=65)&&(sym<=90)) || ((sym>=97)&&(sym<=122))){
+    }
+    else if (((sym >= 65) && (sym <= 90)) || ((sym >= 97) && (sym <= 122))) {
         std::cout << " It's letter" << std::endl;
-    } else {
+    }
+    else {
         std::cout << " It's unknown symbol" << std::endl;
     }
     std::cout << "Input x: ";
@@ -27,19 +23,19 @@ int main() {
     std::cin >> x;
     std::cout << "Result: ";
 
-    switch (x%4){
-        case 0:
-            std::cout << -1 << std::endl;
-            break;
-        case 1:
-            std::cout << pow(x, -1) << std::endl;
-            break;
-        case 2:
-            std::cout << "-" << x << std::endl;
-            break;
-        case 3:
-            std::cout << x << std::endl;
-            break;
+    switch (x % 4) {
+    case 0:
+        std::cout << -1 << std::endl;
+        break;
+    case 1:
+        std::cout << pow(x, -1) << std::endl;
+        break;
+    case 2:
+        std::cout << "-" << x << std::endl;
+        break;
+    case 3:
+        std::cout << x << std::endl;
+        break;
     }
     std::cout << "Input y = ";
     float y;
@@ -47,16 +43,18 @@ int main() {
     float s = sin(y);
     std::cout << "sin(" << y << ") = ";
     std::cout << std::setprecision(7) << s << std::endl;
-    long int kol;
+    long long kol;
     double res;
-    for(auto i = 0.01; i>=0.000001; i*=0.1){
-        kol=-1;
+    for (auto i = 0.01; i >= 0.000001; i *= 0.1) {
+        kol = 0;
         res = 0;
-        while (abs(s - res) > i){
+        fast = 1;
+        while (abs(s - res) > i) {
+            res = res + pow(-1, kol) * pow(y, 2 * kol + 1) / fast;
             ++kol;
-            res = res + pow(-1, kol)*pow(y, 2*kol+1)/factorial(2*kol+1);
+            fast *= 2 * kol * (2 * kol + 1);
         }
-        std::cout << "The accuracy: " << i << " Value of iterations: " << kol+1 << " Result: " << std::setprecision(7) << res << std::endl;
+        std::cout << "The accuracy: " << i << " Value of iterations: " << kol << " Result: " << std::setprecision(7) << res << std::endl;
     }
     return 0;
 }
